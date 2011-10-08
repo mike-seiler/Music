76b0c2c57f56caad85e2510bc73acb9d77e3291c
Mike Seiler <mike.seiler@moat.com>
2011-10-07 21:38:19 -0400

    simple.py properly identifies peak frequency

--- simple.py
+++ simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import scipy
+import sys
 import time
 import pylab
 import pygame, numpy
@@ -9,40 +10,24 @@ import matplotlib.pyplot
 from pygame import sndarray, mixer
 mixer.init()
 
-def autocorr(x,label):
-	returnme = numpy.fft.fftn(x)
-	print label,returnme.argmax()
-	return returnme
-	vals = {}
-	vals[0] = returnme[returnme.argmax()]
-	returnme[returnme.argmax()] = 0
-	#print label,'2',returnme.argmax()
-	vals[1] = returnme[returnme.argmax()]
-	print label,vals[0] / vals[1]
-	return returnme
-
 def print_max(filename):
-	snd = pygame.mixer.Sound(filename)
-	sndArray = pygame.sndarray.array(snd)
-	NpointMultiplier = 2.0
-	new_xp =[(float(x) / NpointMultiplier) for x in range(int(NpointMultiplier*len(sndArray)))] 
-	old_xp = [x for x in range(len(sndArray))]
-	snds = numpy.array(range(len(sndArray)))
-	i = 0
-	for snd in sndArray:
-		snds[i] = snd.mean()
-		i+=1
-	autocorr(snds,filename.split('.')[0]+' not interp')
-	snds = numpy.interp(new_xp, old_xp, snds,0,0)
-	autocorr(snds,filename.split('.')[0])
-	plotme=autocorr(snds,filename.split('.')[0])
-	x = [x for i in range(len(plotme))]
-	t = matplotlib.pyplot.plot(x,plotme)
-	print t
-	matplotlib.pyplot.savefig('test.png')
+  snd = pygame.mixer.Sound(filename)
+  sndArray = pygame.sndarray.array(snd)
+  data = numpy.array(range(len(sndArray)))
+  i = 0
+  for snd in sndArray:
+    data[i] = snd.mean()
+    i+=1
+  plotme = numpy.abs(numpy.fft.fftshift(numpy.fft.fft(data)))
+  plotme = plotme[len(plotme)/2:]
+
+  x = [i for i in range(len(plotme))]
+  t = matplotlib.pyplot.plot(x,plotme)
+  sys.stderr.write( str(plotme.argmax())+"\n")
+  matplotlib.pyplot.savefig('{filename}.png'.format(filename=filename))
 
 
-#print_max('440.wav')
-#print_max('880.wav')
-#print_max('1760.wav')
+print_max('440.wav')
+print_max('880.wav')
+print_max('1760.wav')
 print_max('3520.wav')
